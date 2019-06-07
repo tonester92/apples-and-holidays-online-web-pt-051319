fee7d969e267829ea01b64be82464c35442ce347
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-07 22:17:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,7 +32,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day].push(supply)
 end
 
-add_supply_to_memorial_day(holiday_supplies, "Ballons")
+def add_supply_to_memorial_day(holiday_supplies, "Ballons")
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)

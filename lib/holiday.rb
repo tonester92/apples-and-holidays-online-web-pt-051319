eaf7ab2b2a6b73755e47ceb1d39490716fc4b53d
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-07 22:18:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,25 +24,20 @@ def add_supply_to_winter_holidays(holiday_hash, item)
  holiday_hash[:winter].each do |holiday, decorations|
    decorations << item
   end
-  
 add_supply_to_winter_holidays(holiday_supplies, "Baloons")
-end
+
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day].push(supply)
 end
-
 add_supply_to_memorial_day(holiday_supplies, "Ballons")
-end
+
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[season][holiday_name] = supply_array
-  holiday_hash
 end
-add_new_holiday_with_supplies(holiday_supplies, :fall, :succas, ["lulav", "esrog", "schach"])
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
+  # return an array of all ofendthe supplies that are used in the winter season
 
 end
 
@@ -62,10 +57,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
